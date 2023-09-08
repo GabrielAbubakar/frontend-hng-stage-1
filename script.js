b421c4741ce471a2778456c01ab32a856e32d25e
Gabriel Abubakar <gabrielogrima@gmail.com>
2023-09-08 15:40:09 +0100

    javascript completed

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var week = [null, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-// Calculate the current day of the week and input it
+// Calculate and set the current day of the week
 var dt = new Date()
 var day = dt.getDay()
 document.getElementById('currentDay').innerHTML = week[day]
+
+
+// Calculate and set UTC time
+var time = dt.getTime()
+document.getElementById('currentTime').innerHTML = time
